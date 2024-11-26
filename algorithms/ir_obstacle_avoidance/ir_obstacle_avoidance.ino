e213d78cb269199538dd1133fa77e813b2faa6cc
wasan1alharbi <162335114+wasan1alharbi@users.noreply.github.com>
2024-11-26 15:37:22 +0000

    Update ir_obstacle_avoidance.ino

--- algorithms/ir_obstacle_avoidance/ir_obstacle_avoidance.ino
+++ algorithms/ir_obstacle_avoidance/ir_obstacle_avoidance.ino
@@ -1,22 +1,66 @@
-void setup(){
-   // write initialisation code here
-   pinMode(LED, OUTPUT);
-   pinMode(isObstaclePin, INPUT);
-   Serial.begin(9600); 
+#include "motorboard.h"
+#include "localisation.h"
+#include "hardware_conf.h"
+
+
+#define SPEED 200                      
+#define MILESTONE_LENGTH 20            
+#define OBSTACLE_DISTANCE_THRESHOLD 15 
+
+Motorboard motorboard;
+Localisation loc;
+
+int milestone = 0;
+
+void setup() {
+    motorboard.init(
+        MOTOR_R_POWER_PIN, 
+        MOTOR_L_POWER_PIN,
+        MOTOR_R_FWD_PIN,
+        MOTOR_R_REV_PIN,
+        MOTOR_L_FWD_PIN,
+        MOTOR_L_REV_PIN
+    );
+
+    loc.init(
+        motorboard,
+        ENCODER_R_PIN,
+        ENCODER_L_PIN,
+        WHEEL_RADIUS,
+        LR_WHEEL_DIST,
+        ENCODER_SLIT_NUM
+    );
+
+    pinMode(isObstaclePin, INPUT); 
+    Serial.begin(9600);           
 }
 
-void loop(){
-   // write code that loops here
-   isObstacle = digitalRead(isObstaclePin);
-  if (isObstacle == LOW)
-  {
-    Serial.println("OBSTACLE!!, OBSTACLE!!");
-    digitalWrite(LED, HIGH);
-  }
-  else
-  {
-    Serial.println("clear");
-    digitalWrite(LED, LOW);
-  }
-  delay(200);
+void loop() {
+    int isObstacle = digitalRead(isObstaclePin); 
+
+    if (isObstacle == LOW) { 
+        Serial.println("OBSTACLE DETECTED!");
+        motorboard.stop();       
+        delay(1000);             
+
+        /// Performing obstacle avoidance
+        Serial.println("Turning to avoid obstacle...");
+        motorboard.right(SPEED);
+        delay(1000);            
+        motorboard.forward(SPEED);
+    } else {
+        if (milestone * MILESTONE_LENGTH < loc.y) {
+            Serial.print("Milestone reached at distance: ");
+            Serial.println(loc.y);
+            
+            motorboard.stop();   
+            delay(2000);         
+            
+            milestone++;         
+        }
+
+        motorboard.forward(SPEED);
+    }
+
+    delay(100); 
 }
